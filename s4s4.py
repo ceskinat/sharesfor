3bc280e1e00483b49c6b539a2c862366dd8009eb
ceskinat <ceskinat@gmail.com>
2024-09-13 15:08:06 +0300

    internal documentation for s4s4.py 	modified:   s4s4.py

--- s4s4.py
+++ s4s4.py
@@ -59,23 +59,30 @@ def on_leave(data):
     
 
 @app.route('/routing_form', methods = ["GET", "POST"])
+# main sharesfor route; displays the related objects threads and activethread
 def routing_form():
 
-    if request.method == "GET":
+    if request.method == "GET": # request coming from listed threads
         action = "display routing" #for logging
 
+        # session["user"] comes from the application 
         if not session.get("user") or not session["user"].get("username"):
             abort(401)
 
         otype = request.args.get("otype")
         oid = str(request.args.get("oid"))
+
+        # thread_id == 0 means new (not yet stored in db) thread
         if request.args.get("thread_id") not in ["0", None]:
             active_thr = get_active_thread(request.args.get("thread_id"))
+            # remove user from the unread list since user views this thread 
             remove_from_unread(active_thr["_id"], session["user"])
         else: 
             active_thr = make_new_thread(request.args, session["user"])
         
     else: #POST: request from an application
+
+        # session["user"] comes from the application 
         session.pop("user", None)
         session["user"] = {"userid": request.form.get("userid"),
                            "username": request.form.get("username"),
@@ -83,6 +90,8 @@ def routing_form():
 
         otype = request.form.get("otype")
         oid = str(request.form.get("oid"))
+        # from an application call the new thread is displayed; 
+        # the existing threads are displayed by GET calls
         active_thr = make_new_thread(request.form, session["user"])
     return render_template('s4s4.html', 
                            obj={"type": otype,
@@ -94,13 +103,13 @@ def routing_form():
         
 @app.route('/add_message', methods=["POST"])
 def add_message():
-        
+# add a new message to the thread        
     otype = request.form["otype"]
     oid = request.form["oid"]
 
     if request.form["thread_id"] == "0":  # create new thread
         thread = make_new_thread(request.form, session["user"])
-        action = "route object"
+        action = "route object" # for logging purpose
 
         thr_params = create_rt_thread(request.form["otype"], 
                                       rtg_object_id(request.form["oid"]),
@@ -116,7 +125,7 @@ def add_message():
                                     session["user"]["username"], 
                                       request.form["message"],
                                       request.files,
-                                      "akvaryum")
+                                      "sharesfor")
                                     # signed_msg)
         socketio.emit("refresh", to=str(thr_params["_id"]))
 
@@ -139,20 +148,26 @@ def add_message():
 
 @app.route('/add_audience', methods = [ "POST"])
 def add_audience():
+# add an authorized user to the audience
     
-    if request.form["thread_id"] == "0":
+    if request.form["thread_id"] == "0": 
         activethr = make_new_thread(request.form, session["user"])
     else:
        activethr = get_active_thread(request.form["thread_id"])
        
     otype = request.form["otype"]
     oid = request.form["oid"]
+
+    # the user to be added comes in concatenated id||name||email form
     aud = request.form["slct-aud"].split("||")
     aud = {"id": aud[0], "name": aud[1], "email": aud[2]}
 
 
     thread = add_audience_rtg(activethr, aud)   
     activethr["authorized_users"] = authorized_users(otype, oid)
+
+    # audlist contains only names (to display)
+    # audience is a list of dicts {id, name} 
     activethr["audlist"] = [x["name"] for x in thread["audience"]] 
     # activethr["audience"] = json.dumps(thread["audience"])
     activethr["audience"] = thread["audience"] # using |tojson filter in the template
@@ -168,6 +183,7 @@ def add_audience():
 
 @app.route('/del_audience', methods = [ "POST"])
 def del_audience():
+    # delete the selected user from the audience
 
     thread_id = request.form["thread_id"]
     otype = request.form["otype"]
@@ -202,7 +218,7 @@ def del_audience():
 from routings import add_tag_rtg, del_tag_rtg
 @app.route('/add_tag', methods = [ "POST"])
 def add_tag():
-
+# add the selected tag to the thread
     otype = request.form["otype"]
     oid = request.form["oid"]
     
@@ -224,6 +240,7 @@ def add_tag():
 
 @app.route('/del_tag', methods = [ "POST"])
 def del_tag():
+#remove a tag from the tagslist
     otype = request.form["otype"]
     oid = request.form["oid"]
     otype = request.form["otype"]
@@ -247,6 +264,12 @@ def del_tag():
 
 @app.route('/obj_list', methods=["POST"])
 def obj_list():
+# returns a list of objects in JSON format
+# {id: {id: [otype, oid], 
+#       name: obj_name},
+#  name: display_name}
+
+
 #    from sampleintfcs import object_list
 #    from routings import object_list_html
 #    return object_list_html(request.form.get("name"))
