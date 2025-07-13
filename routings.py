a893a32e384b3dc90ad39b374e32c523a6755858
ceskinat <ceskinat@gmail.com>
2025-07-13 13:10:07 +0300

    bugfix, avoid adding existing tags/subjects 	modified:   routings.py

--- routings.py
+++ routings.py
@@ -392,7 +392,10 @@ def del_audience_rtg(thread, aud):
 
 def add_tag_rtg(thr_params, added_tagstr):
 # add a tag (object) to an existing or new (id="0") thread 
-    thr_params["tags"].append(json.loads(added_tagstr))
+    ats = json.loads(added_tagstr)
+    if ats not in thr_params["tags"]:
+        thr_params["tags"].append(ats)
+
     thr_params["tagslist"] = [x["name"] for x in thr_params["tags"]]
 
     if thr_params["_id"] != "0":
